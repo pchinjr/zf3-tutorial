eeb26bd25a34a1f839cc4b992a21abb3068f4ccc
Paul Chin <pchinjr@gmail.com>
2016-12-27 15:10:20 -0500

    initial commit

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,58 @@
+<?php 
+namespace Album\Controller;
+
+use Album\Form\AlbumForm;
+use Album\Model\Album;
+use Album\Model\AlbumTable;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+     // Add this property:
+    private $table;
+
+    // Add this constructor:
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+    public function indexAction()
+    {
+         return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
+    }
+
+    public function addAction()
+    {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+
+        if (! $request->isPost()) {
+            return ['form' => $form];
+        }
+
+        $album = new Album();
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return ['form' => $form];
+        }
+
+        $album->exchangeArray($form->getData());
+        $this->table->saveAlbum($album);
+        return $this->redirect()->toRoute('album');
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
