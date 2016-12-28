274e5c7f1a2490cf1f1c04e966c408294a37f0fd
Paul Chin <pchinjr@gmail.com>
2016-12-28 00:49:38 -0500

    finished basic tutorial

--- module/Album/src/Model/Album.php
+++ module/Album/src/Model/Album.php
@@ -24,6 +24,14 @@ class Album
         $this->artist = !empty($data['artist']) ? $data['artist'] : null;
         $this->title  = !empty($data['title']) ? $data['title'] : null;
     }
+    public function getArrayCopy()
+    {
+        return [
+            'id'     => $this->id,
+            'artist' => $this->artist,
+            'title'  => $this->title,
+        ];
+    }
     public function setInputFilter(InputFilterInterface $inputFilter)
     {
         throw new DomainException(sprintf(
